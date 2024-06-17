59ef56a87d7eab808fdccbfeb7079dd467551006
Andrei Bazavan <rokyed@gmail.com>
2024-06-17 03:39:02 -0400

    removing wrong build

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,9 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   root: 'src', // specify the directory where index.html is located
   build: {
-    outDir: '../dist', // specify the output directory
+    outDir: '../docs', // specify the output directory
   },
   server: {
     open: true, // automatically open the app in the browser
   },
 });
-
